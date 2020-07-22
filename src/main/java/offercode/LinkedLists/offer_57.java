60d931d6667f3b0e0f2df9cc4e7b44782fc54571
wuda01 <wuda0116@163.com>
2020-07-22 23:22:08 +0800

    20200722

--- src/main/java/offercode/LinkedLists/offer_57.java
+++ src/main/java/offercode/LinkedLists/offer_57.java
@@ -1,5 +1,7 @@
 package offercode.LinkedLists;
 
+import java.util.HashSet;
+
 public class offer_57 {
 
     /**
@@ -8,4 +10,41 @@ public class offer_57 {
      * 重复的结点不保留，返回链表头指针。
      * 例如，链表1->2->3->3->4->4->5 处理后为 1->2->5
      */
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode deleteDuplication(ListNode pHead) {
+
+        if (pHead == null) return null;
+        HashSet<Integer> hashSet = new HashSet<Integer>();
+
+        ListNode pre = pHead;
+        ListNode cur = pHead.next;
+
+        while (cur != null) {
+            if (cur.val == pre.val) hashSet.add(cur.val);
+            pre = cur;
+            cur = cur.next;
+        }
+
+        while (pHead!=null && hashSet.contains(pHead.val)) {
+            pHead = pHead.next;
+        }
+        if (pHead == null) return null;
+
+        pre = pHead;
+        cur = pHead.next;
+        while (cur!=null) {
+            if (hashSet.contains(cur.val)) {
+                pre.next = cur.next;
+                cur = cur.next;
+            } else {
+                pre = cur;
+                cur = cur.next;
+            }
+        }
+        return pHead;
+    }
 }
