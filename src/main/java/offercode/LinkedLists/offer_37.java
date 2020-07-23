88ce5def196cd1e680cbbd4e1e086d8c24e866e7
wuda01 <wuda0116@163.com>
2020-07-23 12:39:07 +0800

    20200722

---
+++ src/main/java/offercode/LinkedLists/offer_37.java
@@ -0,0 +1,59 @@
+package offercode.LinkedLists;
+
+public class offer_37 {
+
+    /**
+     * 两个链表的第一个公共节点
+     * 输入两个链表，找出它们的第一个公共结点。
+     * （注意因为传入数据是链表，所以错误测试数据的提示是用其他方式显示的，保证传入数据是正确的）
+     * 遍历两遍这两个链表，如果有重复的节点，那么一定能够使遍历的指针相等。
+     */
+
+    public static void main(String[] args) {
+
+        ListNode node1 = new ListNode(0);
+        ListNode l2= new ListNode(1);
+        ListNode l3 = new ListNode(2);
+        ListNode l4 = new ListNode(3);
+        ListNode l5 = new ListNode(4);
+        ListNode l6 = new ListNode(5);
+        node1.next = l2;
+        node1.next.next = l3;
+        node1.next.next.next = l4;
+        node1.next.next.next.next = l5;
+        node1.next.next.next.next.next = l6;
+
+        ListNode node2 = new ListNode(7);
+        ListNode l20= new ListNode(8);
+        ListNode l30 = new ListNode(4);
+        ListNode l40 = new ListNode(5);
+        node2.next = l20;
+        node2.next.next = l30;
+        node2.next.next.next = l40;
+
+        /*ListNode p = node1;
+        while (p != null) {
+            System.out.println(p.val);
+            p = p.next;
+        }*/
+        System.out.println(FindFirstCommonNode(node1, node2).val);
+
+    }
+
+    public static ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+
+        if (pHead1==null || pHead2==null) return null;
+
+        ListNode p1 = pHead1;
+        ListNode p2 = pHead2;
+        while (p1.val != p2.val) {
+            p1 = p1.next;
+            p2 = p2.next;
+            if (p1 != p2) {
+                if (p1 == null) p1 = pHead2;
+                if (p2 == null) p2 = pHead1;
+            }
+        }
+        return p1;
+    }
+}
---
+++ src/main/java/offercode/LinkedLists/offer_56.java
@@ -0,0 +1,68 @@
+package offercode.LinkedLists;
+
+public class offer_56 {
+
+    /**
+     * 链表中环的入口节点
+     * 给一个链表，若其中包含环，请找出该链表的环的入口结点，否则，输出null。
+     * ### 如果链表中存在环，经过多次循环之后.next和.next.next会重合
+     * 链表中的环一定在链表结尾（一个指针只能指向一个地址，一个地址可以被多个指针指向）
+     */
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1);
+        ListNode l2= new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        ListNode l6 = new ListNode(6);
+        ListNode l7 = new ListNode(7);
+        ListNode l8 = new ListNode(8);
+        node.next = l2;
+        node.next.next = l3;
+        node.next.next.next = l4;
+        node.next.next.next.next = l5;
+        node.next.next.next.next.next = l6;
+        node.next.next.next.next.next.next = l7;
+        node.next.next.next.next.next.next.next = l8;
+        node.next.next.next.next.next.next.next.next = l3;
+
+        System.out.println(EntryNodeOfLoop(node).val);
+    }
+
+    public static ListNode EntryNodeOfLoop(ListNode pHead) {
+        if (pHead == null) return null;
+        ListNode l=pHead, r=pHead;
+        boolean flag = false;
+        // 找到环
+        while (r!=null && r.next!=null) {
+            l = l.next;
+            r = r.next.next;
+            if (l==r) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            return null;
+        } else {
+            // 得到环中的节点数目
+            int n = 1;
+            r = r.next;
+            while (l!=r) {
+                r=r.next;
+                n++;
+            }
+            // 找到环中的入口节点
+            l = r = pHead;
+            for (int i = 0; i < n; i++) {
+                r = r.next;
+            }
+            while (l != r) {
+                l = l.next;
+                r = r.next;
+            }
+        }
+        return l;
+    }
+}
