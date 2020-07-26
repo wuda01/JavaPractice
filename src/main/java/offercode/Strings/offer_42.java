b8acb7d89bc52c21f69213bbc64790d370e201c3
wuda01 <wuda0116@163.com>
2020-07-26 23:42:01 +0800

    20200726

---
+++ src/main/java/offercode/Strings/offer_42.java
@@ -0,0 +1,24 @@
+package offercode.Strings;
+
+public class offer_42 {
+
+    /**
+     * 左旋转字符串
+     * 汇编语言中有一种移位指令叫做循环左移（ROL），现在有个简单的任务，
+     * 就是用字符串模拟这个指令的运算结果。对于一个给定的字符序列S，
+     * 请你把其循环左移K位后的序列输出。例如，字符序列S=”abcXYZdef”,
+     * 要求输出循环左移3位后的结果，即“XYZdefabc”。是不是很简单？OK，搞定它！
+     */
+
+    public static void main(String[] args) {
+        String str = "abcdefg";
+        System.out.println(LeftRotateString(str,3));
+    }
+
+    public static String LeftRotateString(String str,int n) {
+        if (str==null || n>str.length()) return str;
+
+        String result = str.substring(n)+str.substring(0, n);
+        return result;
+    }
+}
